e512928473fe33728fe8823e59724838252fe8cc
Miguel Cardoso <miguel.cardosobanda@gmail.com>
2025-03-27 18:32:46 -0600

    Import Fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ThemeContextProv from "./context/ThemeContext.jsx";
 import LoginContextProv from "./context/LoginContext.jsx";
 import NameContextProv from "./context/UserNameContext.jsx";
 import IdContextProv from "./context/IdContext.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeContextProv>
